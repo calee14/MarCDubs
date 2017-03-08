b5e6b1054aae6b239801562fc8cf208a64d34d27
calee14 <caplee1000@gmail.com>
2017-03-07 21:56:34 -0800

    First Commit

--- MarCDubs/GameScene.swift
+++ MarCDubs/GameScene.swift
@@ -9,79 +9,38 @@
 import SpriteKit
 import GameplayKit
 
+struct CollisionNames {
+    static let Mario : UInt32 = 0x1 << 1
+    static let Ground : UInt32 = 0x1 << 2
+    static let Coin : UInt32 = 0x1 << 4
+}
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    let Map = JSTileMap(named: "level1.tmx")
+    var Mario = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        Map?.position = CGPoint(x: 0, y: 0)
+        self.addChild(Map!)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        Mario = SKSpriteNode(imageNamed: "Mario1")
+        Mario.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        Mario.size = CGSize(width: 30, height: 45)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        Mario.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: Mario.size.width, height: Mario.size.height))
+        //Assigning 0x1 << 1 to Mario
+        Mario.physicsBody?.categoryBitMask = CollisionNames.Mario
+        //If Mario hits Ground or Coin
+        Mario.physicsBody?.collisionBitMask = CollisionNames.Ground | CollisionNames.Coin
+        //Gives Mario gravity
+        Mario.physicsBody?.affectedByGravity = true
+        self.addChild(Mario)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
