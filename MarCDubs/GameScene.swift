2831e70ec3e1256848de40b5afe50851f8554f64
calee14 <caplee1000@gmail.com>
2017-06-21 19:57:59 -0700

    Last Commit Done for MarCDubs

--- MarCDubs/GameScene.swift
+++ MarCDubs/GameScene.swift
@@ -23,6 +23,7 @@ class GameScene: SKScene {
         
         Map?.position = CGPoint(x: 0, y: 0)
         self.addChild(Map!)
+        
         //Creating Mario/Chang
         Mario = SKSpriteNode(imageNamed: "Mario1")
         Mario.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
@@ -33,9 +34,38 @@ class GameScene: SKScene {
         Mario.physicsBody?.categoryBitMask = CollisionNames.Mario
         //If Mario hits Ground or Coin
         Mario.physicsBody?.collisionBitMask = CollisionNames.Ground | CollisionNames.Coin
+        //Contact Test
+        Mario.physicsBody?.contactTestBitMask = CollisionNames.Ground | CollisionNames.Coin
         //Gives Mario gravity
         Mario.physicsBody?.affectedByGravity = true
         self.addChild(Mario)
+        
+        let groundGroup : TMXObjectGroup = self.Map!.groupNamed("GroundObjects")
+        for i in 0..<groundGroup.objects.count {
+            
+            let groundObject = groundGroup.objects.object(at: i) as! NSDictionary
+            
+            let width = groundObject.object(forKey: "width") as! String
+            let height = groundObject.object(forKey: "height") as! String
+            
+            let wallSize = CGSize(width: CGFloat(Int(width)!), height: CGFloat(Int(height)!))
+            
+            let groundSprite = SKSpriteNode(color: UIColor.clear, size: wallSize)
+            
+            let x = groundObject.object(forKey: "x") as! Int
+            let y = groundObject.object(forKey: "y") as! Int
+            groundSprite.position = CGPoint(x: x + Int(groundGroup.positionOffset.x) + Int(width)! / 2, y: y + Int(groundGroup.positionOffset.y) + Int(height)! / 2)
+            
+            groundSprite.physicsBody = SKPhysicsBody(rectangleOf: wallSize)
+            groundSprite.physicsBody?.categoryBitMask = CollisionNames.Ground
+            groundSprite.physicsBody?.collisionBitMask = CollisionNames.Mario
+            groundSprite.physicsBody?.contactTestBitMask = CollisionNames.Mario
+            
+            groundSprite.physicsBody?.affectedByGravity = false
+            groundSprite.physicsBody?.isDynamic = false
+            self.addChild(groundSprite)
+            
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
